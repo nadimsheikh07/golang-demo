dc50ae774971386e2b033531df4994af29dd6cc0
Nadim Sheikh <nadim.sheikh.07@gmail.com>
2026-09-18 22:15:56 +0530

    add

--- main.go
+++ main.go
@@ -6,26 +6,55 @@ import (
 	"time"
 )
 
-func worker(url string, wg *sync.WaitGroup, resultChan chan string) {
+var images = []string{
+	"https://example.com/image1.jpg",
+	"https://example.com/image2.jpg",
+	"https://example.com/image3.jpg",
+	"https://example.com/image4.jpg",
+	"https://example.com/image5.jpg",
+	"https://example.com/image6.jpg",
+	"https://example.com/image7.jpg",
+	"https://example.com/image8.jpg",
+	"https://example.com/image9.jpg",
+	"https://example.com/image10.jpg",
+}
+
+func worker(jobsChan chan string, wg *sync.WaitGroup, resultChan chan string) {
 	defer wg.Done()
-	time.Sleep(time.Millisecond * 50)
-	fmt.Println("Worker for URL:", url)
-	resultChan <- url
+
+	for job := range jobsChan {
+		time.Sleep(time.Millisecond * 50)
+		resultChan <- job
+	}
 }
 
 func main() {
 	var wg sync.WaitGroup
 	startTime := time.Now()
-	resultChan := make(chan string, 2)
-	wg.Add(2)
-	go worker("https://example.com", &wg, resultChan)
-	go worker("https://example.com", &wg, resultChan)
-	wg.Wait()
+	resultChan := make(chan string, len(images))
+	jobsChan := make(chan string, len(images))
 
-	close(resultChan)
+	var totalWorkers = 5
+
+	for i := 1; i < totalWorkers; i++ {
+		wg.Add(1)
+		go worker(jobsChan, &wg, resultChan)
+
+	}
+
+	go func() {
+		wg.Wait()
+		close(resultChan)
+	}()
+
+	for _, image := range images {
+		jobsChan <- image
+	}
+
+	close(jobsChan)
 
 	for result := range resultChan {
-		fmt.Println("result ", result)
+		fmt.Println("job completed: ", result)
 	}
 
 	fmt.Println("it took", time.Since(startTime), "ms.")
